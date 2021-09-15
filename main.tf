199e778cde8ff5d5d8a376d45ff459d6d153119b
johit786 <83961174+johit786@users.noreply.github.com>
2021-09-15 17:47:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,29 @@
-resource "azurerm_resource_group" "main" {
-    name           = "Johit"
-    location       = "East US"
+provider "aws" {
+  region= "us-east-1"
+    
+}
+
+resource "aws_instance" "Aakash"{
+   ami           = "ami-0742b4e673072066f"
+   instance_type = "t2.micro"
+   tags = {
+    "Name" = "My-server-with-terraform"
+  }
+}
+
+# resource "aws_s3_bucket" "b" {
+#   bucket = "vishwakarma121235"
+#   acl    = "private"
+
+#   tags = {
+#     Name        = "Mybucket"
+#     Environment = "Dev"
+   
+#   }
+#}
+resource "aws_s3_bucket" "this" {
+  count = var.create_bucket ? 1 : 0
+
+  bucket        = var.bucket
+  bucket_prefix = var.bucket_prefix
 }
