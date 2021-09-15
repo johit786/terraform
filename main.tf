dc315f3914bad60cef94b1503ea418ccad4370a7
johit786 <83961174+johit786@users.noreply.github.com>
2021-09-15 17:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "main" {
-    name           = var.resource_group_name
-    location       = var.resource_group_location
+    name           = "Johit"
+    location       = "East US"
 }
